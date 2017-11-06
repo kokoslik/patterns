36cd7f354adab7692caf9ceba87fa75dd56b03ce
kokoslik <nnproshunin@gmail.com>
2017-11-07 00:28:35 +0300

    Added decorator and facade patterns.

--- structural/decorator.cpp
+++ structural/decorator.cpp
@@ -1,4 +1,62 @@
+#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+
+class Pet
+{
+public:
+    virtual ~Pet() = default;
+    virtual string getName() const
+    {
+        return "Pet";
+    }
+};
+
+class Dog: public Pet
+{
+public:
+    virtual string getName() const override
+    {
+        return "Dog";
+    }
+};
+
+class PetDecorator: public Pet
+{
+private:
+    unique_ptr<Pet> inner;
+public:
+    PetDecorator(Pet* inner_):inner(inner_){};
+    virtual string getName() const
+    {
+        inner->getName();
+    }
+};
+
+class IamPetDecorator: public PetDecorator
+{
+public:
+    IamPetDecorator(Pet* inner):PetDecorator(inner){};
+    virtual string getName() const
+    {
+        return "I'm a "+PetDecorator::getName();
+    }
+};
+
+class GoodPetDecorator: public PetDecorator
+{
+public:
+    GoodPetDecorator(Pet* inner):PetDecorator(inner){};
+    virtual string getName() const
+    {
+        return "very good "+PetDecorator::getName();
+    }
+};
+
 int main()
 {
+    unique_ptr<Pet> pet(new IamPetDecorator(new GoodPetDecorator(new Dog)));
+    cout<<pet->getName()<<endl;
     return 0;
 }
--- structural/facade.cpp
+++ structural/facade.cpp
@@ -1,4 +1,51 @@
+#include <iostream>
+#include <string>
+#include <memory>
+using namespace std;
+
+class Pet
+{
+public:
+    virtual ~Pet() = default;
+    virtual string getName() const
+    {
+        return "Pet";
+    }
+};
+
+class Cat: public Pet
+{
+public:
+    virtual string getName() const override
+    {
+        return "Cat";
+    }
+};
+
+class Dog: public Pet
+{
+public:
+    virtual string getName() const override
+    {
+        return "Dog";
+    }
+};
+
+class Facade
+{
+private:
+    Dog dog;
+    Cat cat;
+public:
+    void print() const
+    {
+        cout<<dog.getName()<<" and "<<cat.getName()<<" are friends."<<endl;;
+    }
+};
+
 int main()
 {
+    Facade f;
+    f.print();
     return 0;
 }
