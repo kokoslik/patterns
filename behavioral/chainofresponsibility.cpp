e87aeb83c933d7653e333484a8c04dc1c67b0b89
kokoslik <nnproshunin@gmail.com>
2018-03-02 01:33:13 +0300

    Added chain of responsibility and command patterns.

--- behavioral/chainofresponsibility.cpp
+++ behavioral/chainofresponsibility.cpp
@@ -1,7 +1,66 @@
 #include <iostream>
 using namespace std;
 
+class Chain
+{
+private:
+Chain* next_chain;
+public:
+    explicit Chain(Chain* next_ = nullptr):next_chain(next_){}
+    void setNext(Chain* next_)
+    {
+        next_chain = next_;
+    }
+    virtual void handle(int a, int b) const
+    {
+        next_chain->handle(a,b);
+    }
+    virtual ~Chain() = default;
+};
+
+class CheckZero: public Chain
+{
+public:
+    virtual void handle(int a, int b) const override
+    {
+        if(a==0 || b==0)
+            cout<<"GCD = "<<a+b<<endl;
+        else
+            Chain::handle(a,b);
+    }
+};
+
+class AGreaterThanB : public Chain
+{
+public:
+    AGreaterThanB(Chain* ch): Chain(ch){};
+    virtual void handle(int a, int b) const override
+    {
+        if(a >= b)a=a%b;
+        Chain::handle(a,b);
+    }
+};
+
+class BGreaterThanA : public Chain
+{
+public:
+    BGreaterThanA(Chain* ch): Chain(ch){};
+    virtual void handle(int a, int b) const override
+    {
+        if(b > a)b=b%a;
+        Chain::handle(a,b);
+    }
+};
+
 int main()
 {
+    Chain *x = new CheckZero(), *y = new AGreaterThanB(x), *z = new BGreaterThanA(y);
+    x->setNext(z);
+    int a,b;
+    cin>>a>>b;
+    x->handle(a,b);
+    delete x;
+    delete y;
+    delete z;
     return 0;
 }
--- behavioral/command.cpp
+++ behavioral/command.cpp
@@ -1,7 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <map>
 using namespace std;
+
+class VectorWrapper
+{
+private:
+    vector<int> vec;
+public:
+    VectorWrapper() = default;
+    void push_back()
+    {
+        cout<<"Enter number: ";
+        int a;
+        cin>>a;
+        vec.push_back(a);
+    }
+    void pop_back()
+    {
+        vec.pop_back();
+    }
+    void back()
+    {
+        cout<<vec.back()<<endl;
+    }
+    void print()
+    {
+        for(int i: vec)
+            cout<<i<<' ';
+        cout<<endl;
+    }
+};
+
+class Command
+{
+public:
+    typedef void(VectorWrapper::*Action)();
+    Command(VectorWrapper* vec_, Action act_):vec(vec_),act(act_){}
+    void execute()
+    {
+        (vec->*act)();
+    }
+private:
+    VectorWrapper* vec;
+    Action act;
+};
 
 int main()
 {
+    map <string, Command::Action> cmd{{"push_back",&VectorWrapper::push_back},{"pop_back",&VectorWrapper::pop_back},{"back",&VectorWrapper::back},{"print", &VectorWrapper::print}};
+    VectorWrapper vec;
+    string str;
+    while(true)
+    {
+        cin>>str;
+        if(str=="quit")
+            break;
+        if(cmd.find(str)!=cmd.end())
+        {
+            Command comm(&vec, cmd[str]);
+            comm.execute();
+        }
+    }
     return 0;
 }
