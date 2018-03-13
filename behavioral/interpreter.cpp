fca7b936e20402e9980c776b7a264af98c83be9f
kokoslik <nnproshunin@gmail.com>
2018-03-13 21:31:35 +0300

    Added interpreter and iterator patterns

--- behavioral/interpreter.cpp
+++ behavioral/interpreter.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Expression
+{
+public:
+    virtual ~Expression() = default;
+    virtual double eval() = 0;
+};
+
+class Number: public Expression
+{
+private:
+    double num;
+public:
+    explicit Number(double num_): num(num_){}
+    virtual double eval() override
+    {
+        return num;
+    }
+};
+
+class Plus: public Expression
+{
+private:
+    Expression *left, *right;
+public:
+    Plus(Expression* left_, Expression* right_):left(left_),right(right_){}
+    virtual double eval() override
+    {
+        return left->eval()+right->eval();
+    }
+    ~Plus()
+    {
+        delete left;
+        delete right;
+    }
+};
+
+class Mult: public Expression
+{
+private:
+    Expression *left, *right;
+public:
+    Mult(Expression* left_, Expression* right_):left(left_),right(right_){}
+    virtual double eval() override
+    {
+        return left->eval()*right->eval();
+    }
+    ~Mult()
+    {
+        delete left;
+        delete right;
+    }
+};
 
 int main()
 {
+    Expression *expr = new Mult(new Plus(new Number(2),new Number(3)),new Number(6));
+    cout<<"Answer: "<<expr->eval()<<endl;
+    delete expr;
     return 0;
 }
--- behavioral/iterator.cpp
+++ behavioral/iterator.cpp
@@ -1,7 +1,72 @@
 #include <iostream>
 using namespace std;
 
+class IntList
+{
+private:
+    class Node
+    {
+    public:
+        int num;
+        Node *next;
+        Node(int num_,Node *next_=nullptr):num(num_),next(next_){}
+        ~Node()
+        {
+            if(next!=nullptr)
+                delete next;
+            cout<<"deleted"<<endl;
+        }
+    };
+    Node *head;
+public:
+    explicit IntList():head(nullptr){}
+    ~IntList()
+    {
+        if(head!=nullptr)
+            delete head;
+        cout<<"deleted"<<endl;
+    }
+    void add(int i)
+    {
+        head = new Node(i, head);
+    }
+    class Iterator
+    {
+    private:
+        IntList::Node* nd;
+    public:
+        Iterator(IntList::Node* nd_):nd(nd_){}
+        void operator++(int)
+        {
+            nd=nd->next;
+        }
+        bool operator!=(Iterator it)
+        {
+            return it.nd!=nd;
+        }
+        int& operator*()
+        {
+            return nd->num;
+        }
+    };
+    Iterator begin()
+    {
+        return Iterator(head);
+    }
+    Iterator end()
+    {
+        return Iterator(nullptr);
+    }
+};
+
 int main()
 {
+    IntList a;
+    a.add(3);
+    a.add(2);
+    a.add(1);
+    a.add(5);
+    for(auto it = a.begin();it!= a.end(); it++)
+        cout<<*it<<endl;
     return 0;
 }
